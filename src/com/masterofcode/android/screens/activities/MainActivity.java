110261f1863de8911d4606b2796b318c1da301f5
Dmitry Taryanyk <dmitry.taryanyk@masterofcode.com>
2012-04-19 15:11:38 +0300

    Create android project

---
+++ src/com/masterofcode/android/screens/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.masterofcode.android.screens.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.masterofcode.android.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
